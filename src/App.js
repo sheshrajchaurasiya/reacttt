9935afbcd32933705655bd1f020546c48fda1966
sheshrajchaurasiya <120588700+sheshrajchaurasiya@users.noreply.github.com>
2023-05-15 00:36:20 +0530

    codes added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calculator from './Calculator';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
